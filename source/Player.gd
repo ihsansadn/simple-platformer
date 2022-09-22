2c842c0a3c5e6de80aa1559372649398eba6543e
Ihsan Syauqi Adn <48370501+ihsansadn@users.noreply.github.com>
2022-09-22 20:03:30 +0700

    Update Player.gd

--- source/Player.gd
+++ source/Player.gd
@@ -1,4 +1,27 @@
-extend KinematicBody2D
+extends KinematicBody2D
 
-func h_move(): #horizontal move
-	acceleration(50)
+class_name ActorMove
+
+export var speed = 10
+var screen_size
+func _ready():
+	screen_size = get_viewport_rect().size
+
+var jump = Vector2(0, 10)
+
+#Player movement
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("jump"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
